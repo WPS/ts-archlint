2289664f4993724a07eb92b0fa671ce5845f7181
niklas.kopp <niklas.kopp@gmx.net>
2023-07-07 14:49:04 +0200

    explicit exit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 #!/bin/env node
 import {ArchlintCli} from "./cli/archlint-cli";
+import * as process from "process";
 
 const exitCode = new ArchlintCli().run()
-if (exitCode !== 0) {
-    process.exitCode = exitCode
-}
+process.exit(exitCode)
