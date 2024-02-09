e61fdbb2ee626297dab3efe4c5b333440f2dd2b0
niklas.kopp <niklas.kopp@gmx.net>
2024-02-09 15:33:41 +0100

    this is logged before

--- src/report/result-reporter.ts
+++ src/report/result-reporter.ts
@@ -12,7 +12,6 @@ export class ResultReporter {
 
     private reportViolations(violations: DependencyViolation[]): void {
         if (violations.length === 0) {
-            Logger.info('No violations found.')
             return
         }
 
