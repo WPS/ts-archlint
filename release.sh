562f7c5a9a2f78732952dfd27f6be6fdcd9bca95
niklas.kopp <niklas.kopp@gmx.net>
2024-02-15 14:09:55 +0100

    half manual release script

---
+++ release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eou pipefail
+
+VERSION="$1"
+
+echo "Release with Version $VERSION"
+
+sed -i "s/0.0.0-SNAPSHOT/$VERSION/" package.json
+echo "Building"
+npm run build
+echo "Publishing to npm"
+npm publish
+echo "Tagging git"
+git tag $VERSION
+git checkout HEAD -- package.json
+echo "Release $VERSION done"
